70ab972d535a7e6985c3453060d71ba424eaa556
Mohammed Arbaz <134392076+moarbaz01@users.noreply.github.com>
2025-03-14 11:37:23 +0530

    Update index.tsx

--- src/components/Home/TrendingGames/index.tsx
+++ src/components/Home/TrendingGames/index.tsx
@@ -2,7 +2,9 @@ import GameComponent from "@/components/GameComponent";
 
 const fetchProducts = async () => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/product`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/product`, {
+cache : "no-store"
+});
     if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
